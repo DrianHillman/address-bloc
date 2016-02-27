22fcc4bb93378a645eab49fd212a36fd5d186ff7
Drian Hillman <dhillman24@gmail.com>
2016-02-27 18:43:10 -0500

    Assignment 19 completed | creates the ability to remove entries

--- models/address_book.rb
+++ models/address_book.rb
@@ -9,12 +9,31 @@ class AddressBook
 
   def add_entry(name, phone_number, email)
     index = 0
+
     entries.each do |entry|
       if name < entry.name
         break
       end
       index += 1
     end
+
     entries.insert(index, Entry.new(name, phone_number, email))
+
   end
+
+  def remove_entry(name, phone_number, email)
+    entries.each do |x|
+      if  ( x.name == name ) &&
+          ( x.phone_number == phone_number ) &&
+          ( x.email == email )
+
+          entries.delete(x)
+
+        else
+          "This entry does not exist in the database.\nNote: Be sure that your spelling is correct."
+        end
+    end
+
+  end
+
 end
--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -32,7 +32,17 @@ RSpec.describe AddressBook do
       expect(new_entry.phone_number).to eq('010.012.1815')
       expect(new_entry.email).to eq('augusta.king@lovelace.com')
     end
+  end
 
+  describe "#remove_entry" do
+    let(:book) { AddressBook.new }
+
+    it "removes an entry from the Address Book" do
+      book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+
+      book.remove_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
+      expect(book.entries.size).to eq(0)
+    end
   end
 
 end
