db8f0ddcb1f59ed8e7926a82ddaf4c86cc02a79c
Drian Hillman <dhillman24@gmail.com>
2017-01-12 05:35:49 +0000

    added & implemented UPDATE | ORM Ch5 Complete :white_check_mark:

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -137,20 +137,22 @@ class MenuController
   end
 
   def edit_entry(entry)
+    updates = {}
     print "Updated name: "
     name = gets.chomp
+    updates[:name] = name unless name.empty?
     print "Updated phone number: "
     phone_number = gets.chomp
+    updates[:phone_number] = phone_number unless phone_number.empty?
     print "Updated email: "
     email = gets.chomp
-
-    entry.name = name if !name.empty?
-    entry.phone_number = phone_number if !phone_number.empty?
-    entry.email = email if !email.empty?
+    updates[:email] = email unless email.empty?
+    
+    entry.update_attributes(updates)
     system "clear"
 
     puts "Updated entry:"
-    puts entry
+    puts Entry.find(entry.id)
   end
 
   def entry_submenu(entry)
